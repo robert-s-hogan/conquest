31c4bf3c7b3cc131027bfe7f3f1235a55bde101e
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2023-11-11 08:04:03 -0800

    updated env varaiables

--- src/amplifyConfig.js
+++ src/amplifyConfig.js
@@ -2,12 +2,14 @@ const amplifyConfig = {
   aws_project_region: process.env.NEXT_PUBLIC_AWS_PROJECT_REGION,
   aws_appsync_graphqlEndpoint:
     process.env.NEXT_PUBLIC_AWS_APPSYNC_GRAPHQLENDPOINT,
-  aws_appsync_region: process.env.AWS_APPSYNC_REGION,
-  aws_appsync_authenticationType: process.env.AWS_APPSYNC_AUTHENTICATION_TYPE,
-  aws_appsync_apiKey: process.env.AWS_APPSYNC_APIKEY,
-  aws_cognito_identity_pool_id: process.env.AWS_COGNITO_IDENTITY_POOL_ID,
-  aws_cognito_region: process.env.AWS_COGNITO_REGION,
-  aws_user_pools_id: process.env.AWS_USER_POOLS_ID,
+  aws_appsync_region: process.env.NEXT_PUBLIC_AWS_APPSYNC_REGION,
+  aws_appsync_authenticationType:
+    process.env.NEXT_PUBLIC_AWS_APPSYNC_AUTHENTICATION_TYPE,
+  aws_appsync_apiKey: process.env.NEXT_PUBLIC_AWS_APPSYNC_APIKEY,
+  aws_cognito_identity_pool_id:
+    process.env.NEXT_PUBLIC_AWS_COGNITO_IDENTITY_POOL_ID,
+  aws_cognito_region: process.env.NEXT_PUBLIC_AWS_COGNITO_REGION,
+  aws_user_pools_id: process.env.NEXT_PUBLIC_AWS_USER_POOLS_ID,
   aws_user_pools_web_client_id:
     process.env.NEXT_PUBLIC_AWS_USER_POOLS_WEB_CLIENT_ID,
   oauth: {},
